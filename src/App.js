5347512bbbd65abe6b6e2628468e5dd101f7bad2
kunalgiri <kunalgiri0123@gmail.com>
2023-08-20 12:47:29 +0530

    1. Complete Array Listing with Map function with map with map looping and for loop. 2. Object form data store in table wise.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from './Login';
-
+import React from 'react';
 function App()
 {
+  // map looping
+  // const students = ['Kunal', 'Giri', 'Rohit', 'Aman'];/
+  const students = [
+    {name: "Kunal", email:"kunal@demo.com", contact: 99898989898},
+    {name: "Goswami", email:"goswami@demo.com", contact: 978784555454},
+    {name: "Rohit", email:"rohit@demo.com", contact: 57548787520},
+    {name: "Aman", email:"aman@demo.com", contact: 8787542221},
+  ]
+
   return(
     <div className='App'>
-      <Login />
+      <h1>Handle Array with List</h1>
+      <table className='table-bordered'>
+        <tr>
+          <td>Name</td>
+          <td>Email</td>
+          <td>Contact</td>
+        </tr>
+        {
+          students.map((data)=>
+            <tr>
+              <td>{data.name}</td>
+              <td>{data.email}</td>
+              <td>{data.contact}</td>
+            </tr>
+          )
+        }
+      </table>
     </div>
   )
 }
