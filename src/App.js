5050b68ce2e15f78bf915f19c9e2ecb564096002
kunalgiri <kunalgiri0123@gmail.com>
2023-08-20 13:32:15 +0530

    1. Complete Nested List in react 2. Nested work in list within list show.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,82 @@
 import logo from './logo.svg';
 import './App.css';
 import React from 'react';
-function App()
-{
+import { Table } from 'react-bootstrap';
+function App() {
   // map looping
   // const students = ['Kunal', 'Giri', 'Rohit', 'Aman'];/
-  const students = [
-    {name: "Kunal", email:"kunal@demo.com", contact: 99898989898},
-    {name: "Goswami", email:"goswami@demo.com", contact: 978784555454},
-    {name: "Rohit", email:"rohit@demo.com", contact: 57548787520},
-    {name: "Aman", email:"aman@demo.com", contact: 8787542221},
+  const users = [
+    {
+      name: "Kunal", email: "kunal@demo.com", address: [
+        { Hn: "51", city: "faridabad", country: "India" },
+        { Hn: "26", city: "Noida", country: "India" },
+        { Hn: "45", city: "Delhi", country: "India" },
+        { Hn: "32", city: "Gurgoan", country: "India" },
+      ]
+    },
+    {
+      name: "Goswami", email: "goswami@demo.com", address: [
+        { Hn: "51", city: "faridabad", country: "India" },
+        { Hn: "26", city: "Noida", country: "India" },
+        { Hn: "45", city: "Delhi", country: "India" },
+        { Hn: "32", city: "Gurgoan", country: "India" },
+      ]
+    },
+    {
+      name: "Rohit", email: "rohit@demo.com", address: [
+        { Hn: "51", city: "faridabad", country: "India" },
+        { Hn: "26", city: "Noida", country: "India" },
+        { Hn: "45", city: "Delhi", country: "India" },
+        { Hn: "32", city: "Gurgoan", country: "India" },
+      ]
+    },
+    {
+      name: "Aman", email: "aman@demo.com", address: [
+        { Hn: "51", city: "faridabad", country: "India" },
+        { Hn: "26", city: "Noida", country: "India" },
+        { Hn: "45", city: "Delhi", country: "India" },
+        { Hn: "32", city: "Gurgoan", country: "India" },
+      ]
+    },
   ]
 
-  return(
+  return (
     <div className='App'>
-      <h1>Handle Array with List</h1>
-      <table className='table-bordered'>
-        <tr>
-          <td>Name</td>
-          <td>Email</td>
-          <td>Contact</td>
-        </tr>
-        {
-          students.map((data)=>
-            <tr>
-              <td>{data.name}</td>
-              <td>{data.email}</td>
-              <td>{data.contact}</td>
-            </tr>
-          )
-        }
-      </table>
+      <h1>Nested Array with List</h1>
+      <Table variant='dark' striped>
+        <tbody>
+          <tr>
+            <td>S.N</td>
+            <td>Name</td>
+            <td>Email</td>
+            <td>Address</td>
+          </tr>
+          {
+            users.map((item,i) =>
+              <tr key={i}>
+                <td>{i+1}</td>
+                <td>{item.name}</td>
+                <td>{item.email}</td>
+                <td>
+                  <Table variant='dark' striped>
+                    <tbody>{
+                      item.address.map((data) =>
+                        <tr>
+                          <td>{data.Hn}</td>
+                          <td>{data.city}</td>
+                          <td>{data.country}</td>
+                        </tr>
+                      )}
+                    </tbody>
+                  </Table>
+                </td>
+              </tr>
+            )
+          }
+        </tbody>
+      </Table>
     </div>
-  )
+  );
 }
 
 export default App;
