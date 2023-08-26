9fd9cee414ea1882ea54450cd1bd40801f80390b
kunalgiri <kunalgiri0123@gmail.com>
2023-08-26 22:15:10 +0530

    1. Redux tutorial Start. 2. Redux flow. 3. Redux Flow in One Component to Second Component data transfer with the help of props.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,12 @@
-import React from 'react';
-import { CommonContext } from './components/CommonContext';
-import Main from './components/Main';
-import UpdateButton from './components/UpdateButton';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
-class App extends React.Component {
-  constructor() {
-    super()
-    this.updateColor = (color) => {
-      this.setState({
-        color: color
-      })
-    }
-    this.state = {
-      color: "green",
-      updateColor: this.updateColor
-    }
-  }
-  render() {
-    return (
-      <CommonContext.Provider value={this.state} >
-        <Header />
-        <h1>Complete and easy example</h1>
-        <Main />
-        <UpdateButton />
-        <Footer />
-      </CommonContext.Provider>
-    )
-  }
+import logo from './logo.svg';
+import './App.css';
+import User from './User';
+function App() {
+  return (
+    <div className='App'>
+      <User data={{name: "Kunal Giri", age:24}} />
+    </div>
+  )
 }
 
 export default App;
